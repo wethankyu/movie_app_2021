fb028b1be7e129d57aab25e53b03364d5482e997
kyu <wethankyu@gmail.com>
2021-01-16 01:49:36 +0900

    react 4.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  }; // data that will change, you put it in the state.
+
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });//this.setState({ movies: movies });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+    // setTimeout(() => {
+    //   this.setState({ isLoading: false });
+    // }, 2000);
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return <section class="container">{isLoading
+      ? (
+        <div class="loader">
+          <span class="loader__text">Loading...</span>
+        </div>
+      )
+      : (
+        <div class="movies">
+          {
+            movies.map(movie => (
+              <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+            ))
+          }
+        </div>
+      )}
+    </section>;
+  }
 }
 
 export default App;
