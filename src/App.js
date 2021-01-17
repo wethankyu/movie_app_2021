6059785e645319284748c01f3041f49b007f297a
kyu <wethankyu@gmail.com>
2021-01-17 19:37:10 +0900

    deploy ready

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail"
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  }; // data that will change, you put it in the state.
-
-  getMovies = async () => {
-    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
-    this.setState({ movies, isLoading: false });//this.setState({ movies: movies });
-  };
-
-  componentDidMount() {
-    this.getMovies();
-    // setTimeout(() => {
-    //   this.setState({ isLoading: false });
-    // }, 2000);
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return <section class="container">{isLoading
-      ? (
-        <div class="loader">
-          <span class="loader__text">Loading...</span>
-        </div>
-      )
-      : (
-        <div class="movies">
-          {
-            movies.map(movie => (
-              <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
-            ))
-          }
-        </div>
-      )}
-    </section>;
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>
+        "About page"
+      </span>
+    </div>
+  );
+}
+
+export default About;
